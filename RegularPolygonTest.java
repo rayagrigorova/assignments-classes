caef28b82f8f8fc58fe40b786d34352cc1b7bb1e
raiagrigorova2 <72023155+raiagrigorova2@users.noreply.github.com>
2020-10-30 00:41:32 +0200

    Update RegularPolygonTest.java

--- RegularPolygonTest.java
+++ RegularPolygonTest.java
@@ -5,10 +5,10 @@ public class RegularPolygonTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/*
-		 * Напишете тестова програма, която създава три обекта RegularPolygon, създадени
-		 * с помощта на празния конструктор, използвайки RegularPolygon (6, 4) и
-		 * използвайки RegularPolygon (10, 4, 5.6, 7.8). За всеки обект покажете неговия
-		 * периметър и площ.
+		 * РќР°РїРёС€РµС‚Рµ С‚РµСЃС‚РѕРІР° РїСЂРѕРіСЂР°РјР°, РєРѕСЏС‚Рѕ СЃСЉР·РґР°РІР° С‚СЂРё РѕР±РµРєС‚Р° RegularPolygon, СЃСЉР·РґР°РґРµРЅРё
+		 * СЃ РїРѕРјРѕС‰С‚Р° РЅР° РїСЂР°Р·РЅРёСЏ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ, РёР·РїРѕР»Р·РІР°Р№РєРё RegularPolygon (6, 4) Рё
+		 * РёР·РїРѕР»Р·РІР°Р№РєРё RegularPolygon (10, 4, 5.6, 7.8). Р—Р° РІСЃРµРєРё РѕР±РµРєС‚ РїРѕРєР°Р¶РµС‚Рµ РЅРµРіРѕРІРёСЏ
+		 * РїРµСЂРёРјРµС‚СЉСЂ Рё РїР»РѕС‰.
 		 */
 		
 		RegularPolygon r1 = new RegularPolygon();
@@ -18,6 +18,10 @@ public class RegularPolygonTest {
 		System.out.println("Object 1 : " + r1.getPerimeter() + " " + r1.getArea());
 		System.out.println("Object 2 : " + r2.getPerimeter() + " " + r2.getArea());
 		System.out.println("Object 3 : " + r3.getPerimeter() + " " + r3.getArea());
+		
+		System.out.println("\nException handling:");
+		RegularPolygon r4 = new RegularPolygon(-5, 37843734);
+		RegularPolygon r5 = new RegularPolygon(10, -45, 5.6, 7.8);
 		
 	}
 
